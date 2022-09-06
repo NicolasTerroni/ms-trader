30a62a89aedc3925dcd59bea17dc598945ed1789
nicolasterroni <nsterroni@gmail.com>
2022-09-05 23:34:05 -0300

    Sell gain validation, save date to operations and always save 5 dollars in bnb

--- project/routes/routes.py
+++ project/routes/routes.py
@@ -462,6 +462,7 @@ def make_order():
                 n_last_operation.price = n_price 
                 n_last_operation.quantity = quantity
                 n_last_operation.last_operation = "B"
+                n_last_operation.date = datetime.now()
                 db.session.add(n_last_operation)
                 
 
@@ -515,6 +516,8 @@ def make_order():
                 f_last_operation.price = f_price 
                 f_last_operation.quantity = quantity
                 f_last_operation.last_operation = "B"
+                f_last_operation.date = datetime.now()
+
                 db.session.add(f_last_operation)
 
                 db.session.add(buy)
@@ -553,60 +556,80 @@ def make_order():
             response["N_error"] = {"error": "No se permite vender, la ultima operacion con esta moneda fue una venta."}
         else:
             n_asset_amount = float(n_client.get_asset_balance(asset)['free'])
-            n_last_buy_price = n_last_operation.price
 
-            if float(n_last_buy_price) > float(asset_actual_market_price):
-                response["N_error"] = {"error": "Se esta intentando vender a menos de lo que se gasto al comprar"}
+            # Save 5 usd if the asset is BNB
+            if asset == "BNB":
+                usdt_on_bnb = n_asset_amount * float(asset_actual_market_price)
+                five_bnb_dollars_quantity = 5 * n_asset_amount / usdt_on_bnb
+                n_asset_amount = n_asset_amount - five_bnb_dollars_quantity
 
-            n_params = {
-                'symbol': data['symbol'],
-                'side': 'SELL',
-                'type': 'MARKET',
-                'quantity': n_asset_amount,
-                #'timeInforce': 'IOC',
-                #'price': data['price'],
-                }
-        
-            try:
-                n_order = n_client.create_order(**n_params)
+            
+            # precio guardado de la compra, incluye comisiones pagadas en la compra
+            n_last_buy_price = n_last_operation.price # USDT
+            
+            usdt_sell_price = (n_asset_amount * float(asset_actual_market_price))
 
-                # fill up price
-                n_price = 0
-                for fill in n_order['fills']:
-                    fill_price = float(fill['price']) * float(fill['qty'])
-                    
-                    n_price += fill_price
-                    
-                    n_sell = Sells(
-                        symbol = data['symbol'][:-4],
-                        unit_price = float(fill['price']),
-                        quantity = float(fill['qty']),
-                        commission = float(fill['commission']),
-                        user = "N"
-                        )
-                    db.session.add(n_sell)
+            # COMMISSION
+            asset_commission = (n_asset_amount / 100) * 0.1 
+            usdt_commision = (asset_commission * float(asset_actual_market_price))
 
-                n_last_operation.last_operation = "S"
-                n_last_operation.price = n_price
-                n_last_operation.quantity = n_asset_amount
-                db.session.add(n_last_operation)
-
-                db.session.commit()
-                
-                response['N_order'] = n_order
-
-            except Exception as e: 
+            total_operation_price = usdt_sell_price - usdt_commision
+            
+            if not total_operation_price > n_last_buy_price:
                 response["N_error"] = {
-                    "error": str(e),
-                    "order_params": data,
-                    "asset_actual_market_price": asset_actual_market_price,
-                    "asset_last_buy_price": n_last_buy_price,
-                    "asset_amount": n_asset_amount
-                }
-            finally:
-                
-                n_client.close_connection()
-                print("Client N session closed.")
+                    "error": f"Se esta intentando vender a menos de lo que se pago al comprar",
+                    "buy_price": n_last_buy_price,
+                    "sell_price": total_operation_price}
+            else:
+                n_params = {
+                    'symbol': data['symbol'],
+                    'side': 'SELL',
+                    'type': 'MARKET',
+                    'quantity': n_asset_amount,
+                    }
+            
+                try:
+                    n_order = n_client.create_order(**n_params)
+
+                    # fill up price
+                    n_price = 0
+                    for fill in n_order['fills']:
+                        fill_price = float(fill['price']) * float(fill['qty'])
+                        
+                        n_price += fill_price
+                        
+                        n_sell = Sells(
+                            symbol = data['symbol'][:-4],
+                            unit_price = float(fill['price']),
+                            quantity = float(fill['qty']),
+                            commission = float(fill['commission']),
+                            user = "N"
+                            )
+                        db.session.add(n_sell)
+
+                    n_last_operation.last_operation = "S"
+                    n_last_operation.price = n_price
+                    n_last_operation.quantity = n_asset_amount
+                    n_last_operation.date = datetime.now()
+
+                    db.session.add(n_last_operation)
+
+                    db.session.commit()
+                    
+                    response['N_order'] = n_order
+
+                except Exception as e: 
+                    response["N_error"] = {
+                        "error": str(e),
+                        "order_params": data,
+                        "asset_actual_market_price": asset_actual_market_price,
+                        "asset_last_buy_price": n_last_buy_price,
+                        "asset_amount": n_asset_amount
+                    }
+                finally:
+                    
+                    n_client.close_connection()
+                    print("Client N session closed.")
         
 
 
@@ -616,61 +639,79 @@ def make_order():
             response["F_error"] = {"error": "No se permite vender, la ultima operacion con esta moneda fue una venta."}
         else:
             f_asset_amount = float(f_client.get_asset_balance(asset)['free'])
-            f_last_buy_price = f_last_operation.price
 
-            if float(f_last_buy_price) > float(asset_actual_market_price):
-                response["F_error"] = {"error": "Se esta intentando vender a menos de lo que se gasto al comprar"}
+            # Save 5 usd if the asset is BNB
+            if asset == "BNB":
+                usdt_on_bnb = f_asset_amount * float(asset_actual_market_price)
+                five_bnb_dollars_quantity = 5 * f_asset_amount / usdt_on_bnb
+                f_asset_amount = f_asset_amount - five_bnb_dollars_quantity
 
-            f_params = {
-                'symbol': data['symbol'],
-                'side': 'SELL',
-                'type': 'MARKET',
-                'quantity': f_asset_amount,
-                #'timeInforce': 'IOC',
-                #'price': data['price'],
-                }
-        
-            #import ipdb; ipdb.set_trace()
             
-            try:
-                f_order = f_client.create_order(**f_params)
+            # precio guardado de la compra, incluye comisiones pagadas en la compra
+            f_last_buy_price = f_last_operation.price # USDT
+            
+            usdt_sell_price = (f_asset_amount * float(asset_actual_market_price))
 
-                # fill up price
-                f_price = 0
-                for fill in f_order['fills']:
-                    fill_price = float(fill['price']) * float(fill['qty'])
-                    f_price += fill_price
+            # COMMISSION
+            asset_commission = (f_asset_amount / 100) * 0.1 
+            usdt_commision = (asset_commission * float(asset_actual_market_price))
 
-                
-                    f_sell = Sells(
-                        symbol=data['symbol'][:-4],
-                        unit_price = float(fill['price']),
-                        quantity = float(fill['qty']),
-                        commission = float(fill['commission']),
-                        user = "F"
-                        )
-                    db.session.add(f_sell)
-
-                f_last_operation.last_operation = "S"
-                f_last_operation.price = f_price
-                f_last_operation.quantity = f_asset_amount
-                db.session.add(f_last_operation)
-
-                db.session.commit()
-                response['F_order'] = f_order
-
-
-            except Exception as e: 
+            total_operation_price = usdt_sell_price - usdt_commision
+            
+            if not total_operation_price > f_last_buy_price:
                 response["F_error"] = {
-                    "error": str(e),
-                    "order_params": data,
-                    "asset_actual_market_price": asset_actual_market_price,
-                    "asset_last_buy_price": f_last_buy_price,
-                    "asset_amount": f_asset_amount
-                }
-            finally:
-                f_client.close_connection()
-                print("Client F session closed.")
+                    "error": f"Se esta intentando vender a menos de lo que se pago al comprar",
+                    "buy_price": f_last_buy_price,
+                    "sell_price": total_operation_price}
+            else:
+
+                f_params = {
+                    'symbol': data['symbol'],
+                    'side': 'SELL',
+                    'type': 'MARKET',
+                    'quantity': f_asset_amount,
+                    }
+                
+                try:
+                    f_order = f_client.create_order(**f_params)
+
+                    # fill up price
+                    f_price = 0
+                    for fill in f_order['fills']:
+                        fill_price = float(fill['price']) * float(fill['qty'])
+                        f_price += fill_price
+
+                    
+                        f_sell = Sells(
+                            symbol=data['symbol'][:-4],
+                            unit_price = float(fill['price']),
+                            quantity = float(fill['qty']),
+                            commission = float(fill['commission']),
+                            user = "F"
+                            )
+                        db.session.add(f_sell)
+
+                    f_last_operation.last_operation = "S"
+                    f_last_operation.price = f_price
+                    f_last_operation.quantity = f_asset_amount
+                    f_last_operation.date = datetime.now()
+                    db.session.add(f_last_operation)
+
+                    db.session.commit()
+                    response['F_order'] = f_order
+
+
+                except Exception as e: 
+                    response["F_error"] = {
+                        "error": str(e),
+                        "order_params": data,
+                        "asset_actual_market_price": asset_actual_market_price,
+                        "asset_last_buy_price": f_last_buy_price,
+                        "asset_amount": f_asset_amount
+                    }
+                finally:
+                    f_client.close_connection()
+                    print("Client F session closed.")
                 
         db.session.commit() 
         
